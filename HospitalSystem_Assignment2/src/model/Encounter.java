478e8ad6ef866e7070bc93d6b8226ae32bbe1eb9
BhaktiDesaiB <desai.bhak@northeastern.edu>
2022-10-29 20:14:11 -0400

    gui created

---
+++ HospitalSystem_Assignment2/src/model/Encounter.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author bhakti
+ */
+public class Encounter 
+{
+    
+}
---
+++ HospitalSystem_Assignment2/src/model/PatientDirectory.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ *
+ * @author bhakti
+ */
+public class PatientDirectory 
+{
+   private ArrayList<Patient> patientHistory;
+
+    public ArrayList<Patient> getPatientHistory() {
+        return patientHistory;
+    }
+
+    public void setPatientHistory(ArrayList<Patient> patientHistory) {
+        this.patientHistory = patientHistory;
+    }
+
+   
+}
